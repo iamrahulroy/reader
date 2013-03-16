987e65ae43e3a62989af1917d6a0f9f7ccbb2335
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-03-15 19:52:32 -0700

    We don't need these constants anymore.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 #Rails.application.config.middleware.use OmniAuth::Builder do
 #  #provider :developer unless Rails.env.production?
-#  #provider :facebook, ENV["FACEBOOK_API_KEY"], ENV["FACEBOOK_SECRET"]
+#  #provider :facebook, ENV["FACEBOOK_API_KEY"], ENV["FACEBOOK_API_SECRET"]
 #  #provider :google_oauth2, ENV["GOOGLE_API_KEY"], ENV["GOOGLE_API_SECRET"], {access_type: 'online', approval_prompt: ''}
 #end
