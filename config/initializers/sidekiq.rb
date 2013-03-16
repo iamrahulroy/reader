e7e6435062288143ef6178ba7b23263dacbf52ca
Charlie Wilkins <charliewilkins@gmail.com>
2013-03-15 21:44:57 -0700

    sidekiq me in the nuts.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
   if ENV["DATABASE_URL"]
-    ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.establish_connection "#{ENV["DATABASE_URL"]}?pool=81"
   end
 end
