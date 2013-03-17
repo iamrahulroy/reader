48e1e82a14fe48bd37e4cc58c4c93725f12216f0
Charlie Wilkins <charliewilkins@gmail.com>
2013-03-17 07:24:18 -0700

    move job to icons queue

--- app/workers/update_icon.rb
+++ app/workers/update_icon.rb
@@ -1,6 +1,6 @@
 class UpdateIcon
   include Sidekiq::Worker
-  sidekiq_options :queue => :background
+  sidekiq_options :queue => :icons
   def perform
     UpdateIcon.perform_in 12.hours
     Feed.get_icons
