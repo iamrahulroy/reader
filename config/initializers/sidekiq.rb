73581b2bd16ea61bdcc25ba629de0840ba1c3d8b
Charlie Wilkins <charliewilkins@gmail.com>
2013-03-17 08:18:50 -0700

    remove puts statement

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,6 @@ rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
 rails_env = Rails.env || 'development'
 
-puts "redis url: #{redis_config[rails_env]}"
-
-
 Sidekiq.configure_server do |config|
   config.redis = { :url => redis_config[rails_env], :namespace => 'sidekiq:reader' }
   if ENV["DATABASE_URL"]
