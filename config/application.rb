d44a42451911b8e65332d5efe9807cc411ea494f
Charlie Wilkins <charliewilkins@gmail.com>
2013-03-17 20:27:17 -0700

    .

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ end
 module Reader
   class Application < Rails::Application
 
-    config.threadsafe!
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
