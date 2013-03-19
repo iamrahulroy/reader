682853185355b5e46fbeeca4244650dbec5cfc11
Charlie Wilkins <charliewilkins@gmail.com>
2013-03-18 17:53:17 -0700

    opml imports back in sidekiq

--- app/workers/import_opml.rb
+++ app/workers/import_opml.rb
@@ -1,12 +1,12 @@
 class ImportOpml
+  include Sidekiq::Worker
   include OpmlImporter
-  @queue = :opml
-  def self.perform(filetext, user_id)
-    self.new.import_opml filetext, user_id
+  sidekiq_options :queue => :opml
+  def perform(filetext, user_id)
+    import_opml filetext, user_id
     user = User.find(user_id)
     PlusMailer.opml_imported(user).deliver
   rescue LibXML::XML::Error => e
-
+    ap "OPML parsing error; ignoring"
   end
-
 end
