d0f84d73b60ddb78588c4d5246b648c00403f1e3
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-17 18:21:50 -0700

    Trying out httparty

--- app/services/fetch_feed_service.rb
+++ app/services/fetch_feed_service.rb
@@ -1,20 +1,19 @@
 class FetchFeedService
 
-  attr :url, :etag, :status, :body
+  include HTTParty
+
+  attr :request_url, :url, :etag, :status, :body
   def initialize(options={})
-    @url = options[:url]
-    @etag = options[:etag]
+    @url = @request_url = options[:url]
   end
 
   def perform(options={})
     @url = options[:url] || url
-    @etag = options[:etag] || etag
     @body = nil
 
     response = get_response
 
     if response
-      @etag = response.etag
       @status = response.status
       @url = response.url
       if response.body
@@ -24,43 +23,15 @@ class FetchFeedService
     self
   end
 
-  def self.perform(options)
-    self.new.perform(options)
-  end
-
   protected
 
-  def perform_request(follow = false)
-    Curl::Easy.perform(@url) do |curl|
-      curl.headers["User-Agent"] = "1kpl.us/ruby"
-      curl.headers["If-None-Match"] = @etag if @etag
-      #curl.verbose = true
-      curl.max_redirects = 5
-      curl.timeout = 30
-      curl.follow_location = true if follow
-      curl.on_redirect {|easy,code|
-        @url = location_from_header(easy.header_str) if easy.response_code == 301
-      }
-    end
+  def options
+    {}
   end
 
   def get_response
-    response = perform_request
-
-    if response.response_code == 301
-      response = perform_request(true)
-    end
-    OpenStruct.new(status: response.response_code, body: response.body_str, url: @url, etag: etag_from_header(response.header_str))
-  end
-
-  def location_from_header(header)
-    header =~ /.*Location:\s(.*)\r/
-    $1
-  end
-
-  def etag_from_header(header)
-    header =~ /.*ETag:\s(.*)\r/
-    $1
+    response = self.class.get @url, options
+    OpenStruct.new(status: response.code, body: response.body, url: response.request.last_uri.to_s)
   end
 
 end
---
+++ spec/services/fetch_feed_service_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe FetchFeedService do
+
+  it "should have an attr_reader for url" do
+    fetch = FetchFeedService.new(:url => "http://news.ycombinator.com/rss")
+    expect(fetch.request_url).to eq("http://news.ycombinator.com/rss")
+  end
+
+  it 'returns a body' do
+    fetch = FetchFeedService.new(:url => "http://news.ycombinator.com/rss")
+    fetch.perform
+    expect(fetch.body).to include("<rss version=\"2.0\"><channel><title>Hacker News</title>")
+  end
+
+  it 'has the correct URL when redirected' do
+    fetch = FetchFeedService.new(:url => "http://news.ycombinator.com/rss")
+    fetch.perform
+    expect(fetch.url).to eq("https://news.ycombinator.com/rss")
+  end
+end
