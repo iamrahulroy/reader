18e656b466558e09d2d167371a6c501354edf74b
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-20 16:27:05 -0700

    .

--- spec/features/anonymous_user_spec.rb
+++ spec/features/anonymous_user_spec.rb
@@ -3,7 +3,7 @@ require 'features/spec_acceptance_helper'
 describe "anonymous user visits site", :type => :feature do
   it "default feeds are visible", :js => true, :vcr => {:record => :new_episodes} do
     create_anon_feeds
-    Feed.first.name.should eq "AmazingSuperPowers: Webcomic at the Speed of Light » Comics"
+    Feed.first.name.should include "AmazingSuperPowers: Webcomic at the Speed of Light"
     FeedIcon.count.should eq 2
     visit "/"
     page.should have_content "Comics"
