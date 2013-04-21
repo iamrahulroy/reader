0dd39ac313b1f3054c6051fd6b7672b53fc6a089
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-21 11:19:03 -0700

    .

--- app/workers/update_user_subscriptions.rb
+++ app/workers/update_user_subscriptions.rb
@@ -5,6 +5,7 @@ class UpdateUserSubscriptions
     user = User.find(user_id)
     user.set_weights
     user.subscriptions.each do |sub|
+      sub.update_counts
       PollFeedNow.perform_async(sub.feed.id) if sub.feed
     end
   end
