4e186538ff9180ff83370837ad3234a2bdc1c17f
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-23 08:00:17 -0700

    sidekiq jobs throwing Errno::EMFILE: Too many open files

--- app/workers/poll_feeds_for_active_users.rb
+++ app/workers/poll_feeds_for_active_users.rb
@@ -5,7 +5,10 @@ class PollFeedsForActiveUsers
     users = User.where("last_seen_at > '#{1.week.ago.to_s}'")
     users.each do |user|
       puts "Queuing for #{user.id} - #{user.name}"
-      UpdateUserSubscriptions.perform_async(user.id)
+      user = User.find(user_id)
+      user.subscriptions.each do |sub|
+        PollFeed.perform_async(sub.feed.id) if sub.feed
+      end
     end
     self.class.perform_in(Reader::UPDATE_FREQUENCY.minutes)
   end
