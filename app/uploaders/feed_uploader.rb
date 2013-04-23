3c8ef683dbff4711abaf255099e6522e9091bf30
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-23 06:20:35 -0700

    sidekiq jobs throwing Errno::EMFILE: Too many open files

--- app/uploaders/feed_uploader.rb
+++ app/uploaders/feed_uploader.rb
@@ -14,6 +14,14 @@ class FeedUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  def move_to_cache
+    true
+  end
+
+  def move_to_store
+    true
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
 
