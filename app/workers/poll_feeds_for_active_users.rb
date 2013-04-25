b82e41562fef9fd967a1e71a9e011f85afb51091
Charlie Wilkins <charliewilkins@gmail.com>
2013-04-25 06:52:31 -0700

    sidekiq

--- app/workers/poll_feeds_for_active_users.rb
+++ app/workers/poll_feeds_for_active_users.rb
@@ -4,12 +4,18 @@ class PollFeedsForActiveUsers
   def perform
     self.class.perform_in(Reader::UPDATE_FREQUENCY.minutes)
     users = User.where("last_seen_at > '#{1.week.ago.to_s}'")
+    ids = []
     users.each do |user|
       puts "Queuing for #{user.id} - #{user.name}"
       user = User.find(user.id)
       user.subscriptions.each do |sub|
-        PollFeed.perform_in(1.minute, sub.feed.id) if sub.feed
+        ids << sub.feed.id if sub.feed
       end
+    end
+    ap ids
+    ap "#{ids.length} feeds to poll"
+    ids.uniq.each do |id|
+      PollFeed.perform_in(1.minute, id)
     end
     `god restart reader`
 
