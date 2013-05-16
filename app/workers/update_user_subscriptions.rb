bc1896f49c2a01198bc19ef9594a1f70a2913032
Charlie Wilkins <charliewilkins@gmail.com>
2013-05-15 21:16:56 -0700

    trim down workers, disable on demand polling.

--- app/workers/update_user_subscriptions.rb
+++ app/workers/update_user_subscriptions.rb
@@ -6,7 +6,7 @@ class UpdateUserSubscriptions
     user.set_weights
     user.subscriptions.each do |sub|
       sub.update_counts
-      PollFeedNow.perform_async(sub.feed.id) if sub.feed
+      #PollFeedNow.perform_async(sub.feed.id) if sub.feed
     end
   end
 end
