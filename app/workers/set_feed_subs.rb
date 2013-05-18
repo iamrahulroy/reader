80a9e8bcd1599d20cb620144a0b031a746c1646d
Charlie Wilkins <charliewilkins@gmail.com>
2013-05-18 07:29:51 -0700

    polymorphic subscription source, in progress

--- app/workers/set_feed_subs.rb
+++ app/workers/set_feed_subs.rb
@@ -4,8 +4,10 @@ class SetFeedSubs
   def perform(id)
     feed = Feed.find(id)
     Rails.logger.debug "updating feed subs #{feed.id} - #{feed.name}"
-    feed.subs = feed.subscriptions
-  rescue
-    Rails.logger.debug "SetFeedSubs #{id} error #{$!}"
+    feed.subscriptions.each do |sub|
+      sub.source_id = feed.id
+      sub.source_type = 'Feed'
+      sub.save!
+    end
   end
 end
