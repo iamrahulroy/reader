5d7f2567b97c4490b52fcfe9eda75d2416f53083
Chris Hunt <c@chrishunt.co>
2013-05-31 21:09:38 -0700

    Update schema after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -229,8 +229,8 @@ ActiveRecord::Schema.define(:version => 20130519061622) do
   add_index "subscriptions", ["id"], :name => "subscriptions_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                          :default => ""
-    t.string   "name"
+    t.string   "email",                          :default => "",    :null => false
+    t.string   "name",                                              :null => false
     t.string   "encrypted_password",             :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
