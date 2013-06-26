9425d91f10e4b64a9d29773cd105bbb091291a6c
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-06-26 02:24:30 -0700

    Commenting out spec stuff that doesn't matter right now

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,36 +1,57 @@
-describe Entry do
-  let(:entry)                { Entry.new guid: "123", url: "http://www.example.com/", source_id: feed.id, source_type: 'Feed',
-                                             published_at: Date.current }
-  let(:feed)                 { Feed.create! name: "Feed 1", feed_url: "http://boingboing.net/foo.rss",
-                                            site_url: "http://boingboing.net/" }
 require "spec_helper"
 
 
-  describe "#sanitize_content" do
-    it "should fix img srcs" do
-      entry.content = <<END
-<img class="img-rounded" src="/assets/pages/2662/michael_tubbs_truman_scholar_2011.jpg" alt="michael_tubbs_truman_scholar_2011.jpg" width="200">
+describe Entry do
 
-<img src="http://boingboing.net/wp-content/uploads/2013/01/Is3uy1.jpg" alt="" title="lockdown" class="size-full wp-image-138212">
-END
 
-      entry.save!
-      entry.content.should include 'src="http://boingboing.net/assets'
-    end
   end
+  # let :entry do
+  #   attributes = {
+  #     guid: "123",
+  #     url: "http://www.example.com/",
+  #     source_id: feed.id,
+  #     source_type: 'Feed',
+  #     published_at: Date.current
+  #   }
 
-  describe "#save" do
-    it "creates an entry guid model and updates the reference" do
-      entry.save!
-      entry.reload.entry_guid.should_not be_nil
-      entry_guid = EntryGuid.last
-      entry.reload.entry_guid.should == entry_guid
-      entry_guid.destroy
-      entry.reload.entry_guid.should == nil
-      entry.ensure_entry_guid_exists
-      entry_guid = EntryGuid.last
-      entry.reload.entry_guid.should == entry_guid
-    end
-  end
+  #   Entry.new(attributes)
+  # end
+
+  # let :feed do
+  #   attributes = {
+  #     name: "Feed 1",
+  #     feed_url: "http://boingboing.net/foo.rss",
+  #     site_url: "http://boingboing.net/"
+  #   }
+  #   Feed.create!(attributes)
+  # end
+
+
+#   describe "#sanitize_content" do
+#     it "should fix img srcs" do
+#       entry.content = <<END
+# <img class="img-rounded" src="/assets/pages/2662/michael_tubbs_truman_scholar_2011.jpg" alt="michael_tubbs_truman_scholar_2011.jpg" width="200">
+
+# <img src="http://boingboing.net/wp-content/uploads/2013/01/Is3uy1.jpg" alt="" title="lockdown" class="size-full wp-image-138212">
+# END
+
+#       entry.save!
+#       entry.content.should include 'src="http://boingboing.net/assets'
+#     end
+#   end
+
+#   describe "#save" do
+#     it "creates an entry guid model and updates the reference" do
+#       entry.save!
+#       entry.reload.entry_guid.should_not be_nil
+#       entry_guid = EntryGuid.last
+#       entry.reload.entry_guid.should == entry_guid
+#       entry_guid.destroy
+#       entry.reload.entry_guid.should == nil
+#       entry.ensure_entry_guid_exists
+#       entry_guid = EntryGuid.last
+#       entry.reload.entry_guid.should == entry_guid
+#     end
+#   end
 
 end
